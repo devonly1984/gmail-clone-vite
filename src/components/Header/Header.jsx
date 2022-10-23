77da355b6c514a67a366476437fe437c9d3340f2
Ponca Labor01 <polbr01@safeway.com>
2022-10-23 11:40:48 -0500

    First Commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="header">
+      I am a header
+      <div className="header__left"></div>
+      <div className="header__middle"></div>
+      <div className="header__right"></div>
+    </div>
+  );
+};
+
+export default Header;
